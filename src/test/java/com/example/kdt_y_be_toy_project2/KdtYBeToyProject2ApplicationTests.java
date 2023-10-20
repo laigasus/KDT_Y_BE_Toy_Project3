38f8ae8f3b428208f7e30f3387262ef294fe4b73
Ok JaeOok <okjaeook98@gmail.com>
2023-10-20 14:48:17 +0900

    [Init] 프로젝트 생성
    스펙은 아래와 같음
    - java 17
    - springboot 3.1.x
    - mysql
    
    (cherry picked from commit f3cd81bbb08f67bfd3cc0e2c8455a2e84da649da)

---
+++ src/test/java/com/example/kdt_y_be_toy_project2/KdtYBeToyProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kdt_y_be_toy_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdtYBeToyProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
