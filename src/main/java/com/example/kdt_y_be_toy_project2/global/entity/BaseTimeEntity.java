e886db4c35aa35f8e23ef417c280ebcf3014bcd6
Ok JaeOok <okjaeook98@gmail.com>
2023-10-23 12:13:41 +0900

    [Fix] 시간 공통 엔티티 요소 추가
    엔티티에 추가되지 않았던 내용 추가
    
    (cherry picked from commit edc4144ffe46164ef9bfe76d18864e6880915fff)

---
+++ src/main/java/com/example/kdt_y_be_toy_project2/global/entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.example.kdt_y_be_toy_project2.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ *  각자 같은 Class를 상속받아도 Column Name을 다르게 설정할 수 있는 기술을 찾아보세요
+ *
+ *      대안 1: 각각 필드를 재정의 해 @Column(name= " ") 사용
+ *      대안 2: @AttributeOverride - 해당 엔티티에서는 다른 컬럼명을 사용하고 싶을때 쓰는 어노테이션
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+
+}
