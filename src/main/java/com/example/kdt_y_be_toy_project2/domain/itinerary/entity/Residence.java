66a1113361f0f879e4767014bd215537fc7aa334
liyusang1 <liyusang1@naver.com>
2023-10-23 12:52:06 +0900

    [Fix] Residence 엔티티 @future,@NotNull 어노테이션 오류수정
    (cherry picked from commit acada2cbd140948e0f98d4c4ba5989b011382d0f)

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/itinerary/entity/Residence.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/itinerary/entity/Residence.java
@@ -1,8 +1,6 @@
 package com.example.kdt_y_be_toy_project2.domain.itinerary.entity;
 
 import jakarta.persistence.Embeddable;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,18 +15,13 @@ import java.time.LocalDateTime;
 public class Residence {
 
     @Comment("체류지 장소명")
-    @NotNull(message = "체류지 장소명은 비어있을 수 없습니다")
     private String residenceName;
 
     @Comment("체류지 출발일시")
-    @Future(message ="체류지 출발일시는 미래 날짜여야 합니다" )
-    @NotNull(message = "체류지 출발일시는 비어있을 수 없습니다")
     private LocalDateTime residenceDepartureTime;
 
 
     @Comment("체류지 도착일시")
-    @Future(message ="체류지 도착일시는 미래 날짜여야 합니다" )
-    @NotNull(message = "체류지 도착일시는 비어있을 수 없습니다")
     private LocalDateTime residenceArrivalTime;
 
     @Builder
