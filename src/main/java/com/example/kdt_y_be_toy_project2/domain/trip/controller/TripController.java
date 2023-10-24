5923d7334bff6cbb9a8d6a6ca1b9f1f9624c3d94
liyusang1 <liyusang1@naver.com>
2023-10-24 17:04:37 +0900

    [Feat]TripController 추가
    (POST) addTrip : 여행기록
    (GET) bringAllTrips : 여행전체조회
    (GET) bringTripById : 여행단건조회
    (PATCH) editTrip : 여행정보수정
    
    (cherry picked from commit 793bf7bec7717da6a4a1042d3a4a0a3c991be7e0)

---
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/controller/TripController.java
@@ -0,0 +1,46 @@
+package com.example.kdt_y_be_toy_project2.domain.trip.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/trip")
+@RequiredArgsConstructor
+public class TripController {
+
+    private final TripService tripService;
+
+    // POST
+    // 여행 일정 기록
+    // BODY
+    @PostMapping("")
+    public ResponseEntity<?> addTrip(@RequestBody @Valid TripRequest tripRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(tripService.insertTrip(tripRequest));
+    }
+
+    // GET
+    // 여행 일정 전체조회
+    @GetMapping("")
+    public ResponseEntity<?> bringAllTrips() {
+        return ResponseEntity.status(HttpStatus.OK).body(tripService.selectTrips());
+    }
+
+    // GET
+    // 여행 일정 조회 단건 {id}
+    @GetMapping("/{id}")
+    public ResponseEntity<?> bringTripById(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(tripService.selectTrip(id));
+    }
+
+    // PATCH
+    // 여행 정보 수정 {id}
+    // Body
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> editTrip(@PathVariable long id,
+                                      @RequestBody @Valid TripRequest tripRequest) {
+        return ResponseEntity.status(HttpStatus.OK).body(tripService.updateTrip(id, tripRequest));
+    }
+}
