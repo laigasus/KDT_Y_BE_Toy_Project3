305f7d0adbdf174e630ca490596743082302ef90
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-10-24 14:17:40 +0900

    [Feat] ItineraryRepository 추가 아래의 기능이 구현되어 있음 - 여정 단건 조회 - 여정 다건 조회 - tripId로 여정 조회 구현
    (cherry picked from commit 3650a938ae3c0370ed973231246fe666f9952c29)

---
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/itinerary/repository/ItineraryRepository.java
@@ -0,0 +1,11 @@
+package com.example.kdt_y_be_toy_project2.domain.itinerary.repository;
+
+import com.example.kdt_y_be_toy_project2.domain.itinerary.entity.Itinerary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
+
+    List<Itinerary> findByTripId(Long id);
+}
