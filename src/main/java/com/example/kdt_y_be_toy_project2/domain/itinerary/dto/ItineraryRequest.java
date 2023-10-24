82b7a5e6c2479849572ea4ea44bdf743a840ef1c
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-10-24 16:03:19 +0900

    [Fix] ItineraryRequest 타입 수정
    - ItineraryRequest dto를 record 타입으로 변경했음
    
    (cherry picked from commit 977ebf203b42e499e0ceba6c6fa30463246cc529)

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/itinerary/dto/ItineraryRequest.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/itinerary/dto/ItineraryRequest.java
@@ -1,18 +1,21 @@
 package com.example.kdt_y_be_toy_project2.domain.itinerary.dto;
 
 import com.example.kdt_y_be_toy_project2.domain.itinerary.entity.Accommodation;
+import com.example.kdt_y_be_toy_project2.domain.itinerary.entity.Itinerary;
 import com.example.kdt_y_be_toy_project2.domain.itinerary.entity.Residence;
 import com.example.kdt_y_be_toy_project2.domain.itinerary.entity.Transportation;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ItineraryRequest {
-
-    private Residence residence;
-    private Accommodation accommodation;
-    private Transportation transportation;
+public record ItineraryRequest(
+        @NotNull Residence residence,
+        @NotNull Accommodation accommodation,
+        @NotNull Transportation transportation
+) {
+    public Itinerary toEntity() {
+        return Itinerary.builder()
+                .residence(this.residence)
+                .accommodation(this.accommodation)
+                .transportation(this.transportation)
+                .build();
+    }
 }
