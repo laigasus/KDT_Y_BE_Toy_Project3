a15dc31deb77fedce61085ab0e32392c848f9976
Alex Kim <alexkjh2011@gmail.com>
2023-10-24 17:42:08 +0900

    [Feat] Exception 핸들링이 가능한 TripControllerAdvice 추가
    (cherry picked from commit f3b740cb53bd0cf6fc1b7cefc6fd181a07920f3f)

---
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/controller/TripControllerAdvice.java
@@ -0,0 +1,71 @@
+package com.example.kdt_y_be_toy_project2.domain.trip.controller;
+
+import com.example.kdt_y_be_toy_project2.domain.trip.error.*;
+import com.example.kdt_y_be_toy_project2.global.exception.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class TripControllerAdvice {
+
+    @ExceptionHandler(value = {TripNotInsertedException.class})
+    public ResponseEntity<ErrorMessage> tripNotInsertedException(Exception exception) {
+        ErrorMessage message = new ErrorMessage(
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(value = TripNotLoadedException.class)
+    public ResponseEntity<ErrorMessage> tripNotLoadedException(Exception exception) {
+        ErrorMessage message = new ErrorMessage(
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = TripNotUpdatedException.class)
+    public ResponseEntity<ErrorMessage> tripNotUpdatedException(Exception exception) {
+        ErrorMessage message = new ErrorMessage(
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = NotValidTripNameException.class)
+    public ResponseEntity<ErrorMessage> notValidTripException(Exception exception) {
+        ErrorMessage message = new ErrorMessage(
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = TripServiceUnavailable.class)
+    public ResponseEntity<ErrorMessage> tripServiceUnavailable(Exception exception) {
+        ErrorMessage message = new ErrorMessage(
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(message, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(value = TripRequestTimeoutException.class)
+    public ResponseEntity<ErrorMessage> tripRequestTimeoutException(Exception exception) {
+        ErrorMessage message = new ErrorMessage(
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(message, HttpStatus.REQUEST_TIMEOUT);
+    }
+}
