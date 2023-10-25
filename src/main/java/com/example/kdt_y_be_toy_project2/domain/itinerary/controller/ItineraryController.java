90d674d4b4e789b36c7ef88ee97b239ef21029e1
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-10-25 17:39:13 +0900

    [Feat] ItineraryController 구현
    아래의 기능을 포함함
    - 여정 다건 기록
    - 여행 일정 조회
    - 여행 일정 단건 조회
    - 여정 정보 수정
    
    (cherry picked from commit 46b311d416e7008281a084c383ba6b3052421086)

---
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/itinerary/controller/ItineraryController.java
@@ -0,0 +1,43 @@
+package com.example.kdt_y_be_toy_project2.domain.itinerary.controller;
+
+import com.example.kdt_y_be_toy_project2.domain.itinerary.dto.ItineraryRequest;
+import com.example.kdt_y_be_toy_project2.domain.itinerary.service.ItineraryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/itinerary")
+@RequiredArgsConstructor
+public class ItineraryController {
+
+    private final ItineraryService itineraryService;
+
+    // POST 여정 기록 다건
+    @PostMapping("{id}")
+    public ResponseEntity<?> addItineraries(@PathVariable long id, @RequestBody @Valid List<ItineraryRequest> itineraryRequestList) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(itineraryService.insertItineraries(id, itineraryRequestList));
+    }
+
+    // GET 여행 일정 조회 다건
+    @GetMapping("")
+    public ResponseEntity<?> bringItineraries() {
+        return ResponseEntity.status(HttpStatus.OK).body(itineraryService.selectItineraries());
+    }
+
+    // GET 여행 일정 조회 단건 {id}
+    @GetMapping("{id}")
+    public ResponseEntity<?> bringItinerary(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(itineraryService.selectItinerary(id));
+    }
+
+    // PATCH 여정 정보 수정 {id}
+    @PatchMapping("{id}")
+    public ResponseEntity<?> editItinerary(@PathVariable long id, @RequestBody @Valid ItineraryRequest itineraryRequest) {
+        return ResponseEntity.status(HttpStatus.OK).body(itineraryService.updateItinerary(id, itineraryRequest));
+    }
+}
