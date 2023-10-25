e39a96dd4d251711118f66aa984db46810c5be81
Ok JaeOok <okjaeook98@gmail.com>
2023-10-26 03:47:45 +0900

    [Fix] Trip 엔티티 재구성
    `#` TripSchedule -> TimeSchedule 적용
    
    (cherry picked from commit 8bda69a70abf15022778a72efadfe1119a54fc3c)

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/entity/Trip.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/entity/Trip.java
@@ -3,12 +3,14 @@ package com.example.kdt_y_be_toy_project2.domain.trip.entity;
 import com.example.kdt_y_be_toy_project2.domain.itinerary.entity.Itinerary;
 import com.example.kdt_y_be_toy_project2.domain.trip.dto.TripRequest;
 import com.example.kdt_y_be_toy_project2.global.entity.BaseTimeEntity;
+import com.example.kdt_y_be_toy_project2.global.entity.TimeSchedule;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,19 +18,19 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Comment("여행")
 public class Trip extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "trip_id", updatable = false, nullable = false)
     @Comment("여행 번호(PK)")
-    private Long id;
+    private Long tripId;
 
     @Comment("여행 이름")
     private String tripName;
 
     @Embedded
-    private TripSchedule tripSchedule;
+    private TimeSchedule timeSchedule;
 
     @Enumerated(EnumType.STRING)
     @Comment("여행 국내/외 여부")
@@ -38,16 +40,16 @@ public class Trip extends BaseTimeEntity {
     private final List<Itinerary> itineraries = new ArrayList<>();
 
     @Builder
-    private Trip(String tripName, TripSchedule tripSchedule, TripDestinationEnum tripDestinationEnum) {
+    private Trip(Long tripId, String tripName, TimeSchedule timeSchedule, TripDestinationEnum tripDestinationEnum) {
+        this.tripId = tripId;
         this.tripName = tripName;
-        this.tripSchedule = tripSchedule;
+        this.timeSchedule = timeSchedule;
         this.tripDestinationEnum = tripDestinationEnum;
     }
 
-    // 여행 정보 수정
-    public void modifyInfo(TripRequest tripRequest){
-        this.tripName = tripRequest.getTripName();
-        this.tripSchedule = tripRequest.getTripSchedule();
-        this.tripDestinationEnum = tripRequest.getTripDestinationEnum();
+    public void modifyInfo(TripRequest tripRequest) {
+        this.tripName = tripRequest.tripName();
+        this.timeSchedule = tripRequest.timeSchedule();
+        this.tripDestinationEnum = tripRequest.tripDestinationEnum();
     }
 }
