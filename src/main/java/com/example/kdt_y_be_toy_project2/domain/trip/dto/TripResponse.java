a6effe08a62818dca44da1eb4cf3c211aecbd672
Leeyusang <65541248+liyusang1@users.noreply.github.com>
2023-10-25 10:04:45 +0900

    [Feat]TripResponse 추가 (#33)
    itineraryList에 가져온 값 할당 필요
    
    (cherry picked from commit 4092043e70b4b82e120627428d644c23cc32f92a)

---
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/dto/TripResponse.java
@@ -0,0 +1,31 @@
+package com.example.kdt_y_be_toy_project2.domain.trip.dto;
+
+import com.example.kdt_y_be_toy_project2.domain.itinerary.entity.Itinerary;
+import com.example.kdt_y_be_toy_project2.domain.trip.entity.Trip;
+import com.example.kdt_y_be_toy_project2.domain.trip.entity.TripDestinationEnum;
+import com.example.kdt_y_be_toy_project2.domain.trip.entity.TripSchedule;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TripResponse {
+    private Long id;
+    private String tripName;
+    private TripSchedule tripSchedule;
+    private TripDestinationEnum tripDestinationEnum;
+    private List<Itinerary> itineraryList;
+
+    public static TripResponse fromEntity(Trip trip){
+        return TripResponse.builder()
+                .id(trip.getId())
+                .tripName(trip.getTripName())
+                .tripSchedule(trip.getTripSchedule())
+                .tripDestinationEnum(trip.getTripDestinationEnum())
+                .build();
+    }
+}
