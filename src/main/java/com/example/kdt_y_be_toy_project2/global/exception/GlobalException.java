995b7fa5a75a3acaf239fb2a8e1603380ba52ebb
WestSilver99 <seo606101@naver.com>
2023-10-25 12:15:12 +0900

    [feat] GlobalException 전역예외처리 추가
    (cherry picked from commit 57fe028cb90443d1e5c1fc59d2ecef70f78a588e)

---
+++ src/main/java/com/example/kdt_y_be_toy_project2/global/exception/GlobalException.java
@@ -0,0 +1,7 @@
+package com.example.kdt_y_be_toy_project2.global.exception;
+
+public class GlobalException extends RuntimeException {
+    public GlobalException(String message) {
+        super(message);
+    }
+}
