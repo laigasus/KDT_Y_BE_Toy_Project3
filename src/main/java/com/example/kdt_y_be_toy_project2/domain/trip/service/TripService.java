839ef81632a3079e9a94b37bec7dbf3c17ed2ee6
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-10-26 12:46:08 +0900

    [Fix] Tripservice 클래스 예외 처리 추가
    - selectTrip에 TripNotLoadedException 추가
    - updateTrip에 TripNotLoadedException 추가
    
    (cherry picked from commit 3646fcfb336fe84409beec5c4620f01c3fe24a75)

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/service/TripService.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/service/TripService.java
@@ -2,7 +2,9 @@ package com.example.kdt_y_be_toy_project2.domain.trip.service;
 
 import com.example.kdt_y_be_toy_project2.domain.trip.dto.TripRequest;
 import com.example.kdt_y_be_toy_project2.domain.trip.dto.TripResponse;
+import com.example.kdt_y_be_toy_project2.domain.trip.error.TripNotLoadedException;
 import com.example.kdt_y_be_toy_project2.domain.trip.repository.TripRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,13 +12,10 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class TripService {
 
     private final TripRepository tripRepository;
-
-    public TripService(TripRepository tripRepository) {
-        this.tripRepository = tripRepository;
-    }
 
     @Transactional
     public TripResponse insertTrip(TripRequest tripRequest) {
@@ -34,7 +33,7 @@ public class TripService {
     public TripResponse selectTrip(long id) {
         return tripRepository.findById(id)
                 .map(TripResponse::fromEntity)
-                .orElse(null);
+                .orElseThrow(TripNotLoadedException::new);
     }
 
     @Transactional
@@ -42,6 +41,6 @@ public class TripService {
         return tripRepository.findById(id).map(trip -> {
             trip.modifyInfo(tripRequest);
             return TripResponse.fromEntity(trip);
-        }).orElse(null);
+        }).orElseThrow(TripNotLoadedException::new);
     }
 }
