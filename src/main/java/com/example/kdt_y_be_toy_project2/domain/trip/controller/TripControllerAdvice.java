142726f82726132414a2d6d5a580b4d346b20d1f
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-10-26 15:29:00 +0900

    [Fix] TripControllerAdvice에 유효하지 않은 JSON 파일 예외처리 추가
    (cherry picked from commit 51683d53534412531faaec9e2dae5de6a12b9d46)

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/controller/TripControllerAdvice.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/controller/TripControllerAdvice.java
@@ -4,6 +4,8 @@ import com.example.kdt_y_be_toy_project2.domain.trip.error.*;
 import com.example.kdt_y_be_toy_project2.global.exception.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +13,15 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class TripControllerAdvice {
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(Exception exception) {
+        ErrorMessage message = new ErrorMessage(
+                "유효하지 않은 JSON 형태 입니다.",
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(value = {TripNotInsertedException.class})
     public ResponseEntity<ErrorMessage> tripNotInsertedException(Exception exception) {
