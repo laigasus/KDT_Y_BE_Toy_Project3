3b049af307ed9fb407b56532970d1bcd01a47c45
liyusang1 <liyusang1@naver.com>
2023-11-11 18:47:08 +0900

    [Feat] TripComment getTripComment 추가
    TripComment getTripComment 기능 추가

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/comment/dto/TripCommentGetResponse.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/comment/dto/TripCommentGetResponse.java
@@ -10,17 +10,21 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record TripCommentGetResponse(
-        @Positive Long tripCommentId,
-        @NotNull Trip trip,
-        @NotNull User user,
-        @NotBlank String tripComment,
+        @NotNull Long tripCommentId,
+        @NotNull Long tripId,
+        @NotNull String tripName,
+        @NotNull String userEmail,
+        @NotNull String username,
+        @NotNull String tripComment,
         @Future String updatedAt
 ) {
     public static TripCommentGetResponse fromEntity(TripComment tripComment) {
         return new TripCommentGetResponse(
                 tripComment.getTripCommentId(),
-                tripComment.getTrip(),
-                tripComment.getUser(),
+                tripComment.getTrip().getTripId(),
+                tripComment.getTrip().getTripName(),
+                tripComment.getUser().getEmail(),
+                tripComment.getUser().getUsername(),
                 tripComment.getTripComment(),
                 TimeUtils.formatDateTime(tripComment.getUpdatedAt())
         );
