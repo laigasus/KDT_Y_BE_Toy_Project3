d0f6b2bb0e24dd6cb53376294d6c1b8f36b60d48
liyusang1 <liyusang1@naver.com>
2023-11-11 18:18:58 +0900

    [Feat] TripComment postTripComment추가
    TripComment postTripComment추가

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/comment/dto/TripCommentAddRequest.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/comment/dto/TripCommentAddRequest.java
@@ -3,15 +3,13 @@ package com.example.kdt_y_be_toy_project2.domain.comment.dto;
 import com.example.kdt_y_be_toy_project2.domain.comment.entity.TripComment;
 import com.example.kdt_y_be_toy_project2.domain.trip.entity.Trip;
 import com.example.kdt_y_be_toy_project2.domain.user.entity.User;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
+import java.util.Optional;
 
 public record TripCommentAddRequest(
-        @NotNull Trip trip,
-        @NotBlank User user,
-        @NotBlank String tripComment
+        String tripComment
 ) {
-    public TripComment toEntity() {
+    public TripComment toEntity(User user,Trip trip) {
         return TripComment.builder()
                 .trip(trip)
                 .user(user)
