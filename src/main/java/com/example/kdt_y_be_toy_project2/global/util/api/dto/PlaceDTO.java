1c4ea170bec8951a71b1aaed387e334a86797f0a
WestSilver99 <seo606101@naver.com>
2023-11-13 12:00:03 +0900

    [feat] #74 구글 API로 위치정보 표현

--- src/main/java/com/example/kdt_y_be_toy_project2/global/util/api/dto/PlaceDTO.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/global/util/api/dto/PlaceDTO.java
@@ -4,17 +4,15 @@ import lombok.Data;
 
 @Data
 public class PlaceDTO {
-    private String adrAddress;
     private String formattedAddress;
-    private String formattedPhoneNumber;
+    //private String internationalPhoneNumber;
     private String name;
 
 
     public static PlaceDTO fromPlace(Place place) {
         PlaceDTO placeDTO = new PlaceDTO();
-        placeDTO.setAdrAddress(place.getAdr_address());
         placeDTO.setFormattedAddress(place.getFormatted_address());
-        placeDTO.setFormattedPhoneNumber(place.getFormatted_phone_number());
+       // placeDTO.setInternationalPhoneNumber(place.getInternational_phone_number());
         placeDTO.setName(place.getName());
 
 
--- src/main/java/com/example/kdt_y_be_toy_project2/global/util/api/dto/PlacesSearchStatus.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/global/util/api/dto/PlacesSearchStatus.java
@@ -1,10 +1,5 @@
 package com.example.kdt_y_be_toy_project2.global.util.api.dto;
 
 public enum PlacesSearchStatus {
-    OK, // 요청이 성공적으로 완료됨
-    ZERO_RESULTS, // 검색에 대한 결과가 없음
-    OVER_QUERY_LIMIT, // 할당량 초과
-    REQUEST_DENIED, // 요청이 거부됨
-    INVALID_REQUEST, // 잘못된 요청
-    UNKNOWN_ERROR // 알 수 없는 오류
+    OK, ZERO_RESULTS, OVER_QUERY_LIMIT, REQUEST_DENIED, INVALID_REQUEST, UNKNOWN_ERROR
 }
