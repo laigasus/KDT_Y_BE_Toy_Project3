edfd958a344a012aab6b86b656ce06c5e769c098
WestSilver99 <seo606101@naver.com>
2023-11-14 16:05:26 +0900

    [Fix] #5 API KEY 해결완료 => dotenv-java dependency 추가 안쓰는 lombok 제거! Service => GoogleMapUtils로 변경 Controller에서 GoogleMapUtils는 static으로써 사용가능

--- src/main/java/com/example/kdt_y_be_toy_project2/global/util/api/controller/GoogleController.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/global/util/api/controller/GoogleController.java
@@ -1,11 +1,11 @@
 package com.example.kdt_y_be_toy_project2.global.util.api.controller;
 
 import com.example.kdt_y_be_toy_project2.global.util.api.dto.Place;
-import com.example.kdt_y_be_toy_project2.global.util.api.service.GoogleService;
+import com.example.kdt_y_be_toy_project2.global.util.api.service.GoogleMapUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,11 +14,9 @@ import java.util.List;
 @AllArgsConstructor
 public class GoogleController {
 
-    private final GoogleService googleService;
-
-    @GetMapping("/places/search")
-    public ResponseEntity<List<Place>> searchPlaces(@RequestParam String keyword) {
-        List<Place> places = googleService.searchPlaces(keyword);
+    @GetMapping("/places/search/{keyword}")
+    public ResponseEntity<List<Place>> searchPlaces(@PathVariable String keyword) {
+        List<Place> places = GoogleMapUtils.searchPlaces(keyword);
         return ResponseEntity.ok(places);
     }
 }
