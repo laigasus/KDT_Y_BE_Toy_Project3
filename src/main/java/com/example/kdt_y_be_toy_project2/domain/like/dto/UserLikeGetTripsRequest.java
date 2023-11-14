7dbae0c5d35d271102748902824d1a2e6a7b26be
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-11-14 16:03:55 +0900

    [Chore] UserLikeGetTripsRequest 삭제

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/like/dto/UserLikeGetTripsRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.kdt_y_be_toy_project2.domain.like.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record UserLikeGetTripsRequest(
-        @NotNull Long tripId,
-        @NotNull String email
-) {
-}
