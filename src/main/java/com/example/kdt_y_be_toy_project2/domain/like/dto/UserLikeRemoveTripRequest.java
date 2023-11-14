afd493fbe511683006afe35a023bab2aeb723204
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-11-14 15:59:19 +0900

    [Fix] UserLikeRemoveTripRequest에 eamil 필드 삭제
    - 로그인 방식이 변경됨에 따라 email 필드가 불필요함

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/like/dto/UserLikeRemoveTripRequest.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/like/dto/UserLikeRemoveTripRequest.java
@@ -1,10 +1,8 @@
 package com.example.kdt_y_be_toy_project2.domain.like.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UserLikeRemoveTripRequest(
-        @NotNull Long tripId,
-        @NotBlank String email
+        @NotNull Long tripId
 ) {
 }
