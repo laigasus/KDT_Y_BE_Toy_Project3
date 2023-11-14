3c452a40b4027aaf78d5934e5ad73fa07903a42c
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-11-14 16:10:34 +0900

    [fix] UserLikeController 수정
    - 로그인 방식 변경됨에 따라 JWT를 통해 User를 가져오는 방식으로 수정
    - 메서드에 가져온 User 추가

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/like/controller/UserLikeController.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/like/controller/UserLikeController.java
@@ -3,36 +3,43 @@ package com.example.kdt_y_be_toy_project2.domain.like.controller;
 import com.example.kdt_y_be_toy_project2.domain.like.dto.UserLikeAddTripRequest;
 import com.example.kdt_y_be_toy_project2.domain.like.dto.UserLikeRemoveTripRequest;
 import com.example.kdt_y_be_toy_project2.domain.like.service.UserLikeService;
+import com.example.kdt_y_be_toy_project2.domain.user.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/userlike")
+@RequestMapping("/userLike")
 @RequiredArgsConstructor
 public class UserLikeController {
     private final UserLikeService userLikeService;
 
     @PostMapping("")
     public ResponseEntity<?> addUserLike(
-            @RequestBody @Valid UserLikeAddTripRequest userLikeAddTripRequest
-            //TODO: jwt에서 userId가져와서 같이 넣어줘야함
-    ){
-        return ResponseEntity.ok(userLikeService.addUserLike(userLikeAddTripRequest));
+            @RequestBody @Valid UserLikeAddTripRequest userLikeAddTripRequest,
+            Authentication authentication
+    ) {
+        User user = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(userLikeService.addUserLike(user, userLikeAddTripRequest.tripId()));
     }
 
 
-    @GetMapping("{userId}")
-    public ResponseEntity<?> getUserLike(@PathVariable final long userId){
-        return ResponseEntity.ok(userLikeService.bringUserLike(userId));
+    @GetMapping("")
+    public ResponseEntity<?> getUserLike(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(userLikeService.bringUserLike(user));
     }
 
-    @DeleteMapping("{userId}")
+    @DeleteMapping("")
     public ResponseEntity<?> removeUserLike(
-            @RequestBody @Valid UserLikeRemoveTripRequest userLikeRemoveTripRequest
-    ){
-        userLikeService.removeUserLike(userLikeRemoveTripRequest);
+            @RequestBody @Valid UserLikeRemoveTripRequest userLikeRemoveTripRequest,
+            Authentication authentication
+    ) {
+
+        User user = (User) authentication.getPrincipal();
+        userLikeService.removeUserLike(user, userLikeRemoveTripRequest.tripId());
         return ResponseEntity.noContent().build();
     }
 
