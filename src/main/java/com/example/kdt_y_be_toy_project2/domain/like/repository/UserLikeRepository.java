34e3b902b352bc9d51a9a217a3d2000387b2db41
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-11-14 16:20:39 +0900

    [fix] UserLikeRepository 수정
    - deleteByUserEmailAndTripTripId 반환 타입 변경
    - 유저 조회 추가

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/like/repository/UserLikeRepository.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/like/repository/UserLikeRepository.java
@@ -1,9 +1,14 @@
 package com.example.kdt_y_be_toy_project2.domain.like.repository;
 
 import com.example.kdt_y_be_toy_project2.domain.like.entity.UserLike;
+import com.example.kdt_y_be_toy_project2.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserLikeRepository extends JpaRepository<UserLike, Long>{
-    boolean deleteByUserEmailAndTripTripId(String email, Long tripId);
+import java.util.List;
+import java.util.Optional;
 
+public interface UserLikeRepository extends JpaRepository<UserLike, Long> {
+    Optional<UserLike> findByUserAndTripTripId(User user, Long tripId);
+
+    List<UserLike> findByUser(User user);
 }
