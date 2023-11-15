89925757611c9f93b91da18f39b9d9988c111099
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-11-15 15:48:01 +0900

    [Feat] TripRepository 추가
    - keyWord로 여행 검색하는 코드 추가

--- src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/repository/TripRepository.java
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/trip/repository/TripRepository.java
@@ -4,5 +4,8 @@ package com.example.kdt_y_be_toy_project2.domain.trip.repository;
 import com.example.kdt_y_be_toy_project2.domain.trip.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TripRepository extends JpaRepository<Trip, Long> {
+    List<Trip>findTop10ByTripNameContaining(String keyword);
 }
