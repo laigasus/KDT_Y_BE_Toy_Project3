4dc90a3ed5049be177025e766a32beb759b4bcd0
liyusang1 <liyusang1@naver.com>
2023-11-15 10:33:01 +0900

    [Feat] signup시 중복된 email 관련 에러 처리 코드 추가
    signup시 중복된 email로 시도 할때 관련 에러 처리 코드 추가

---
+++ src/main/java/com/example/kdt_y_be_toy_project2/domain/user/controller/UserControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.example.kdt_y_be_toy_project2.domain.user.controller;
+
+import com.example.kdt_y_be_toy_project2.domain.user.error.EmailDuplicateError;
+import com.example.kdt_y_be_toy_project2.global.exception.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class UserControllerAdvice {
+    @ExceptionHandler(value = {
+            EmailDuplicateError.class
+    })
+    public ResponseEntity<ErrorMessage> handleEmailDuplicateError(
+            EmailDuplicateError exception
+    ) {
+        ErrorMessage errorMessage = new ErrorMessage(
+                exception.getMessage(),
+                LocalDateTime.now());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+}
